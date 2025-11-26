2e781815e802968258f46e16f3576f0e04d9f8e6
MAbuhanif <techabyssinia2@gmail.com>
2025-11-26 10:14:43 +0000

    feat: Add initial Inertia.js setup including root view and middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -12,7 +12,7 @@ class HandleInertiaRequests extends Middleware
      *
      * @var string
      */
-    protected $rootView = 'app';
+    protected $rootView = 'root';
 
     /**
      * Determine the current asset version.
